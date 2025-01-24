911765358c0734c9ef66c21b25b5201240e1ca85
Nebuchadnezzar-I <onur.orrmenn@gmail.com>
2025-01-24 16:03:12 +0100

    Added 3 pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -42,4 +42,68 @@ onMount(() => {
             src="https://framerusercontent.com/images/2UROuyS0Fm2VW1duGwNSLEgUzEg.png"
             />
     </div>
+
+    <div class="flex justify-end my-[4rem] px-4">
+        <div class="w-full h-full flex flex-col gap-[4rem] max-w-[1200px]">
+            <h3 class="
+                text-[24px]
+                lg:text-[32px]
+                xl:text-[40px]
+                max-w-[70%] font-medium
+                ">
+                We are a digital agency from Canada. We are a small and dedicated team of skilled and passionate people.
+            </h3>
+
+            <p class="
+                font-medium
+                text-[14px]
+                lg:text-[16px]
+                xl:text-[20px]
+                ">
+                [ EXPERIENCED PEEPS ]
+            </p>
+
+            <div class="flex flex-col gap-6 font-medium
+                ">
+                <div class="border-[#2d2d2d] border-t">
+                    <p class="
+                        text-[18px] lg:text-[20px] xl:text-[24px] text-red-500
+                        relative top-[10px] lg:top-[20px]
+                        ">YEARS OF EXPERIENCE</p>
+                    <h1 class="
+                        font-extrabold inter
+                        text-[128px] leading-[128px]
+                        lg:text-[216px] lg:leading-[216px]
+                        xl:text-[250px] xl:leading-[250px]
+                        ">
+                        17
+                    </h1>
+                </div>
+
+                <div class="border-[#2d2d2d] border-t">
+                    <p class="
+                        text-[18px] lg:text-[20px] xl:text-[24px] text-red-500
+                        relative top-[10px] lg:top-[20px]
+                        ">TEAM MEMBERS</p>
+                    <h1 class="
+                        font-extrabold inter
+                        text-[128px] leading-[128px]
+                        lg:text-[216px] lg:leading-[216px]
+                        xl:text-[250px] xl:leading-[250px]
+                        ">
+                        25
+                    </h1>
+                </div>
+            </div>
+            
+            <p class="
+                font-medium
+                text-[14px]
+                lg:text-[16px]
+                xl:text-[20px]
+                ">
+                Where Creativity<br />meets the technology.
+            </p>
+        </div>
+    </div>
 </div>
--- src/routes/career/+page.svelte
+++ src/routes/career/+page.svelte
@@ -47,4 +47,185 @@ onMount(() => {
             src="https://framerusercontent.com/images/PBQLn1d0kYRVINNCtpW1ENJ6Gkw.png"
             />
     </div>
+
+    <div class="flex justify-end my-[4rem] px-4">
+        <div class="w-full h-full flex flex-col gap-[4rem] max-w-[1200px]">
+            <h3 class="
+                text-[24px]
+                lg:text-[32px]
+                xl:text-[40px]
+                max-w-[70%] font-medium
+                ">
+                Join a community of design and technology leaders
+            </h3>
+
+            <div class="flex flex-col gap-6">
+                <p class="
+                    font-medium
+                    text-[14px]
+                    lg:text-[16px]
+                    xl:text-[20px]
+                    ">
+                    [ INTERESTED IN OPPORTUNITIES? ]
+                </p>
+
+                <p class="
+                    font-medium
+                    text-[14px]
+                    lg:text-[16px]
+                    xl:text-[20px]
+                    max-w-[500px]
+                    ">
+                    Learn more about what brings us together and motivates us on a daily basis, the recruitment process and your future teammates.
+                </p>
+
+                <p class="
+                    font-medium
+                    text-[14px]
+                    lg:text-[16px]
+                    xl:text-[20px]
+                    max-w-[500px]
+                    ">
+                    We are always looking for experienced and talented people to join our team.
+                </p>
+
+                <p class="
+                    mt-[4rem]
+                    font-medium
+                    text-[14px]
+                    lg:text-[16px]
+                    xl:text-[20px]
+                    ">
+                    [ CAREERS ]
+                </p>
+            </div>
+
+            <div class="flex flex-col font-medium">
+                <div class="flex items-center justify-between py-10">
+                    <div class="flex flex-col lg:flex-row items-center gap-6 justify-between">
+                        <p class="text-[18px] lg:text-[20px] xl:text-[24px] text-red-500">
+                            01
+                        </p>
+                        <h1 class="
+                            font-medium inter
+                            text-[22px] leading-[22px]
+                            md:text-[40px] md:leading-[40px]
+                            lg:text-[45px] lg:leading-[45px]
+                            xl:text-[55px] xl:leading-[55px]
+                            ">
+                            Lead Designer
+                        </h1>
+                    </div>
+                    <h1 class="
+                        font-medium inter
+                        text-[18px] leading-[18px]
+                        lg:text-[20px] lg:leading-[20px]
+                        xl:text-[24px] xl:leading-[24px]
+                        ">
+                        Remote first & Paris — Permanent contract
+                    </h1>
+                </div>
+
+                <div class="border-[#2d2d2d] border-t flex items-center justify-between py-10">
+                    <div class="flex flex-col lg:flex-row items-center gap-6 justify-between">
+                        <p class="text-[18px] lg:text-[20px] xl:text-[24px] text-red-500">
+                            02
+                        </p>
+                        <h1 class="
+                            font-medium inter
+                            text-[22px] leading-[22px]
+                            md:text-[40px] md:leading-[40px]
+                            lg:text-[45px] lg:leading-[45px]
+                            xl:text-[55px] xl:leading-[55px]
+                            ">
+                            Freelance Developer
+                        </h1>
+                    </div>
+                    <h1 class="
+                        font-medium inter
+                        text-[18px] leading-[18px]
+                        lg:text-[20px] lg:leading-[20px]
+                        xl:text-[24px] xl:leading-[24px]
+                        ">
+                        Remote first & Paris — Freelance
+                    </h1>
+                </div>
+
+                <div class="border-[#2d2d2d] border-t flex items-center justify-between py-10">
+                    <div class="flex flex-col lg:flex-row items-center gap-6 justify-between">
+                        <p class="text-[18px] lg:text-[20px] xl:text-[24px] text-red-500">
+                            03
+                        </p>
+                        <h1 class="
+                            font-medium inter
+                            text-[22px] leading-[22px]
+                            md:text-[40px] md:leading-[40px]
+                            lg:text-[45px] lg:leading-[45px]
+                            xl:text-[55px] xl:leading-[55px]
+                            ">
+                            Business Analyst
+                        </h1>
+                    </div>
+                    <h1 class="
+                        font-medium inter
+                        text-[18px] leading-[18px]
+                        lg:text-[20px] lg:leading-[20px]
+                        xl:text-[24px] xl:leading-[24px]
+                        ">
+                        Permanent contract
+                    </h1>
+                </div>
+
+                <div class="border-[#2d2d2d] border-t flex items-center justify-between py-10">
+                    <div class="flex flex-col lg:flex-row items-center gap-6 justify-between">
+                        <p class="text-[18px] lg:text-[20px] xl:text-[24px] text-red-500">
+                            04
+                        </p>
+                        <h1 class="
+                            font-medium inter
+                            text-[22px] leading-[22px]
+                            md:text-[40px] md:leading-[40px]
+                            lg:text-[45px] lg:leading-[45px]
+                            xl:text-[55px] xl:leading-[55px]
+                            ">
+                            UI / UX Designer
+                        </h1>
+                    </div>
+                    <h1 class="
+                        font-medium inter
+                        text-[18px] leading-[18px]
+                        lg:text-[20px] lg:leading-[20px]
+                        xl:text-[24px] xl:leading-[24px]
+                        ">
+                        On-Site — Paris Studio
+                    </h1>
+                </div>
+
+                <div class="border-[#2d2d2d] border-t flex items-center justify-between py-10">
+                    <div class="flex flex-col lg:flex-row items-center gap-6 justify-between">
+                        <p class="text-[18px] lg:text-[20px] xl:text-[24px] text-red-500">
+                            05
+                        </p>
+                        <h1 class="
+                            font-medium inter
+                            text-[22px] leading-[22px]
+                            md:text-[40px] md:leading-[40px]
+                            lg:text-[45px] lg:leading-[45px]
+                            xl:text-[55px] xl:leading-[55px]
+                            ">
+                            UI Designer
+                        </h1>
+                    </div>
+                    <h1 class="
+                        font-medium inter
+                        text-[18px] leading-[18px]
+                        lg:text-[20px] lg:leading-[20px]
+                        xl:text-[24px] xl:leading-[24px]
+                        ">
+                        Remote first & Paris — Freelance
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
